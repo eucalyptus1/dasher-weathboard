3dd7678776432a56c7d45bf8a163f24de557b2d2
James Harrington <jamesharrington585@hotmail.com>
2022-12-10 16:51:14 -0500

    can't get lat/lon from dom so used another openweather api

--- script.js
+++ script.js
@@ -6,14 +6,36 @@ var key = "c118df7a58d35d00efaf3e629f196896";
 
 
 
+
 function searchCity(event) {
     event.preventDefault();
     var currentCity = searchInput.value;
-    console.log(currentCity);
-    return currentCity
-    
+
+    fetchLocation(currentCity);
 
 
 };
+
+function fetchLocation(currentCity) {
+  var api = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&appid=${key}`
+  fetch (api)
+  .then(function(response) {
+    var data = response.json();
+    console.log(data);
+    return data;
+  })
+}
+
+// function fetchWeather(currentCity) {
+//    var api = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
+//    fetch (api)
+//    .then(function(response) {
+
+//     var data = response.json();
+//     console.log(data);
+//     return data;
+//    })
+
+// }
 
 searchBtn.addEventListener("click", searchCity);
